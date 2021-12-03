ad2c88ad4c31bb2fe6ab25b61d3efb806ea59911
Sikap <49215307+Sikap@users.noreply.github.com>
2021-12-03 17:20:54 -0500

    it works for 5 register limit but not 3 register limit

--- TP4/src/analyzer/visitors/PrintMachineCodeVisitor.java
+++ TP4/src/analyzer/visitors/PrintMachineCodeVisitor.java
@@ -270,57 +270,41 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         // TODO: if var is a constant (starts with '#'), return var
         if (var.charAt(0)=='#') {return var;}
         // TODO: if REGISTERS contains var, return "R"+index
-        if (REGISTERS.contains(var)) {return ("R" + REGISTERS.indexOf(var));}
+        if (REGISTERS.contains(var)) {return ("R"+ REGISTERS.indexOf(var));}
         // TODO: if REGISTERS size is not max (<REG), add var to REGISTERS and return "R"+index
-        if(REGISTERS.size()<REG){
+        if (REGISTERS.size() < REG) {
             REGISTERS.add(var);
-            MODIFIED.add("");
-            if(var.charAt(0) != 't')
-                m_writer.println("LD" + " R" +  REGISTERS.indexOf(var) + ", " +  var);
-            return ("R" + REGISTERS.indexOf(var));
+            if(load_if_not_found)
+                return ("R"+ REGISTERS.indexOf(var));
         }
         // TODO: if REGISTERS has max size,
         //          put var in space of an other variable which is not used anymore
         //          or
         //          put var in space of var which as the largest next-use
-        else if (REGISTERS.size() == REG) {
-            Integer maxEntry = -1;
-            int entryIndex = -1;
-            //put var in space of an other variable which is not used anymore
-            for(String reg_variable : REGISTERS){
-                if(!life.contains(reg_variable) || (!next.nextuse.containsKey(reg_variable) && load_if_not_found)){
-                    entryIndex = REGISTERS.indexOf(reg_variable);
-                   /*m_writer.println("LD" + " R" +  entryIndex + ", " +  var);
-                    REGISTERS.set(entryIndex, var);*/
+         else if (REGISTERS.size() == REG) {
+            int max = 0;
+            int index = 0;
+            for (int i = 0; i < REGISTERS.size(); i++) {
+                if (!life.contains(REGISTERS.get(i))) {
+                    index = i;
+                    break;
+                }
+                ArrayList<Integer> nextUse = next.nextuse.get(REGISTERS.get(i));
+                if (nextUse == null) {
+                    index = i;
+                    break;
+                }
+                if (nextUse.get(nextUse.size() - 1) >= max) {
+                    max = nextUse.get(nextUse.size() - 1);
+                    index = i;
                 }
             }
-            if(entryIndex == -1){
-                //Case spill
-                // put var in space of var which as the largest next-use
-                // Search for max next use
-                for(String nextUseVar : next.nextuse.keySet()){
-                    ArrayList<Integer> arrayNextUseValues = next.nextuse.get(nextUseVar);
-                    if(REGISTERS.contains(nextUseVar)){
-                        for (Integer arrayNextUseValue : arrayNextUseValues) {
-                            if (arrayNextUseValue > maxEntry) {
-                                maxEntry = arrayNextUseValue;
-                                entryIndex = REGISTERS.indexOf(nextUseVar);
-                            }
-                        }
-                    }
-                }
-                if(entryIndex == -1)
-                    entryIndex = 0; //If not in next use and not in life we choose arbitrary register
-
-                if(MODIFIED.contains(var)){
-                    m_writer.println("ST " + REGISTERS.get(entryIndex) + ", " + "R" + entryIndex );
-                    MODIFIED.set(entryIndex, "");
-                }
+            if (MODIFIED.contains(REGISTERS.get(index)) && life.contains(REGISTERS.get(index))) {
+                m_writer.println("ST "+ REGISTERS.get(index) + ", R" + index);
+                MODIFIED.remove(REGISTERS.get(index));
             }
-            if(var.charAt(0) != 't')
-                m_writer.println("LD" + " R" + entryIndex + ", " +  var);
-            REGISTERS.set(entryIndex, var);
-            return("R" + entryIndex);
+            REGISTERS.set(index, var);
+            return ("R" + index);
         }
         return null;
     }
@@ -330,25 +314,44 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
 
         for (int i = 0; i < CODE.size(); i++) {
             m_writer.println("// Step " + i);
-            //Hashmap string pour build output ou string builder
-            String left_reg = "";
-            String right_reg= "";
-            String assign_reg= "";
-            left_reg = choose_register(CODE.get(i).LEFT, CODE.get(i).Life_IN, CODE.get(i).Next_OUT, false);
-            right_reg = choose_register(CODE.get(i).RIGHT, CODE.get(i).Life_IN, CODE.get(i).Next_OUT, false);
-            assign_reg = choose_register(CODE.get(i).ASSIGN, CODE.get(i).Life_IN, CODE.get(i).Next_OUT, true);
-            int test1 = REGISTERS.indexOf(CODE.get(i).ASSIGN);
-            String test2 = CODE.get(i).ASSIGN;
-            MODIFIED.set(REGISTERS.indexOf(CODE.get(i).ASSIGN), CODE.get(i).ASSIGN);
-            if (left_reg.charAt(0)!='#')
-                m_writer.println(CODE.get(i).OP + " " + assign_reg + ", " + left_reg + ", " + right_reg);
-            m_writer.println(CODE.get(i));
+            String leftVar = CODE.get(i).LEFT;
+            String rightVar = CODE.get(i).RIGHT;
+            String assignVar = CODE.get(i).ASSIGN;
+            String leftReg;
+            if(REGISTERS.contains(leftVar)) {
+                leftReg = choose_register(leftVar, CODE.get(i).Life_IN, CODE.get(i).Next_IN, false);
+            }else{
+                leftReg = choose_register(leftVar, CODE.get(i).Life_IN, CODE.get(i).Next_IN, true);
+                if(leftReg.charAt(0) !='#') {
+                    m_writer.println("LD " + leftReg + ", " + leftVar);
+                }
+            }
+            String rightReg;
+            if(REGISTERS.contains(rightVar)) {
+                rightReg = choose_register(rightVar, CODE.get(i).Life_IN, CODE.get(i).Next_IN, false);
+            }else{
+                rightReg = choose_register(rightVar, CODE.get(i).Life_IN, CODE.get(i).Next_IN, true);
+                if(rightReg.charAt(0) !='#') {
+                    m_writer.println("LD " + rightReg + ", " + rightVar);
+                }
+            }
+            String assignReg = " ";
+            if(REGISTERS.contains(assignVar)) {
+                assignReg = choose_register(assignVar, CODE.get(i).Life_OUT, CODE.get(i).Next_OUT, false);
+            }else{
+                assignReg = choose_register(assignVar, CODE.get(i).Life_OUT, CODE.get(i).Next_OUT, true);
+            }
+            boolean isZero = (leftReg.equals("#0") || rightReg.equals("#0")) && (CODE.get(i).OP.equals("ADD") || CODE.get(i).OP.equals("SUB"));
+            if (!((assignReg.equals(leftReg) || assignReg.equals(rightReg)) && isZero)) {
+                m_writer.println(CODE.get(i).OP + " " + assignReg + ", " + leftReg + ", " + rightReg);
+            }
+            MODIFIED.add(CODE.get(i).ASSIGN);
+            m_writer.println(CODE.get(i).toString());
         }
 
-        //Get les var de retour des registres si présentes dans ceux-ci
-        for (String var_to_return : RETURNED) {
-            if(REGISTERS.contains(var_to_return))
-                m_writer.println("ST " + var_to_return + ", " + " R" + REGISTERS.indexOf(var_to_return));
+        for (int i = 0; i < REGISTERS.size(); i++){
+            if (MODIFIED.contains(REGISTERS.get(i)) && RETURNED.contains(REGISTERS.get(i)))
+                m_writer.println("ST "+ REGISTERS.get(i) + ", R" + i);
         }
     }
 
